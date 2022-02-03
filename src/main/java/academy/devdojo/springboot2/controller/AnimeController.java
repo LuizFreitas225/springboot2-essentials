2cbb6f38c55cb79b59246cba2dbe5856f6281dfe
LuizFreitas225 <luiznicolaulemosfreitas@gmail.com>
2022-02-03 10:46:01 -0300

    first commit

---
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -0,0 +1,43 @@
+package academy.devdojo.springboot2.controller;
+
+import academy.devdojo.springboot2.Service.AnimeService;
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("animes")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+    private  final AnimeService animeService;
+
+   //http://localhost:8080/anime
+    @GetMapping
+    public ResponseEntity<List<Anime>> list() {
+        log.info(dateUtil.fomartLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return  ResponseEntity.ok(animeService.listAll());
+
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        log.info(dateUtil.fomartLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return  ResponseEntity.ok(animeService.findById(id));
+
+    }
+}
